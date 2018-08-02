5bfa80fc6ffcd362ecf4732663d8cd7f1623b7b0
Tokazama <zchristensen7@gmail.com>
2018-08-02 13:57:22 -0400

    First commit

---
+++ BaseModel.py
@@ -0,0 +1,32 @@
+from collections import OrderedDict
+
+class BaseModel:
+    def __init__(self, data, **kwargs):
+        self.data = data
+        self.steps = OrderedDict(**kwargs)
+
+    def run():
+        """Runs/executes the model
+        """
+
+class BaseStep:
+    def __init__(self):
+        pass
+
+    def function:
+        pass
+
+    def params:
+        pass
+
+    def result:
+        pass
+
+    def run:
+        pass
+
+class 
+"""
+def make_step(function, params, result):
+    pass
+"""
